834a556849c6c9e5a30269cdb05bdde1157794a6
Leandro Barreto <leandrobarreto.barreto321@gmail.com>
2025-01-03 18:48:06 -0300

    criar o logger

--- src/main/java/tech/buildrun/btgpactual/orderms/listener/OrderCreatedListener.java
+++ src/main/java/tech/buildrun/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -1,5 +1,7 @@
 package tech.buildrun.btgpactual.orderms.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -7,13 +9,13 @@ import tech.buildrun.btgpactual.orderms.dto.OrderCreatedEvent;
 
 import static tech.buildrun.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
 
-
 @Component
 public class OrderCreatedListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
-    public void listen(Message<OrderCreatedEvent> message){
-
-
+    public void listen(Message<OrderCreatedEvent> message) {
+        logger.info("Message consumed: {}", message);
     }
 }
