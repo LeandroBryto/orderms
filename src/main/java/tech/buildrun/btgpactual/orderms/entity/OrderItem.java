d1d80300d4fa5376c157f2154028782bb9133f11
Leandro Barreto <leandrobarreto.barreto321@gmail.com>
2025-01-04 18:46:42 -0300

    criado service

--- src/main/java/tech/buildrun/btgpactual/orderms/entity/OrderItem.java
+++ src/main/java/tech/buildrun/btgpactual/orderms/entity/OrderItem.java
@@ -1,21 +1,27 @@
 package tech.buildrun.btgpactual.orderms.entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.math.BigDecimal;
-
+@AllArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
 public class OrderItem {
 
     private String product;
     private Integer quantity;
     @Field(targetType = FieldType.DECIMAL128)
     private BigDecimal price;
+
+    public OrderItem(Integer quantity, String product, BigDecimal price) {
+        this.quantity = quantity;
+        this.product = product;
+        this.price = price;
+    }
+
 }
